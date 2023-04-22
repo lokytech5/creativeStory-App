2c5501e5ed77ba6ce645dbc7d9d67c0d0fc88219
lokytech <lokytech5@gmail.com>
2023-04-22 04:14:22 +0100

    "Added dotenv package to make api key hidden"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <>
-
+      <h1>Story app</h1>
     </>
   );
 }
---
+++ src/pages/generateStory.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const OPENAI_API_URL = process.env.OPENAI_API_URL
