cae47e74a9610ef68643254bdac06df9fb2794a3
lokytech <lokytech5@gmail.com>
2023-04-22 04:20:34 +0100

    "Added generateStory post request code to OpenAI"

--- src/pages/generateStory.js
+++ src/pages/generateStory.js
@@ -2,3 +2,29 @@ import axios from "axios";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const OPENAI_API_URL = process.env.OPENAI_API_URL
+
+//*This function takes in prompt as input, token and temperature as parameters
+async function generateStory(prompt, tokens = 150, temperature = 0.8){
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
+    };
+
+
+    const data = {
+        prompt: prompt,
+        max_tokens: tokens,
+        n: 1,
+        temperature: temperature
+    };
+
+    //*Making a post request with the required parameters listed above
+    try {
+        const response = await axios.post(OPENAI_API_URL, data, { headers: headers });
+        return response.data.choices[0].text;
+    } catch (error) {
+        console.error('Error generating story:', error);
+        return null;
+    }
+}
